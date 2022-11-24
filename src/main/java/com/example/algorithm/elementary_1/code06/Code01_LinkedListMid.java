55484760b7fbc193f6e63816691e2edabc5b55fa
admin <www.461189416@qq.com>
2022-11-24 10:04:25 +0800

    快慢指针

--- src/main/java/com/example/algorithm/elementary_1/code06/Code01_LinkedListMid.java
+++ src/main/java/com/example/algorithm/elementary_1/code06/Code01_LinkedListMid.java
@@ -1,97 +1,98 @@
 package com.example.algorithm.elementary_1.code06;
 
 public class Code01_LinkedListMid {
-    public static void main(String[] args) {
-        Node node = null;
-        node = new Node(1);
-        node.next = new Node(2);
-        node.next.next = new Node(3);
-        node.next.next.next = new Node(4);
-        node.next.next.next.next = new Node(5);
-        node.next.next.next.next.next = new Node(6);
-        node.next.next.next.next.next.next = new Node(7);
-        node.next.next.next.next.next.next.next = new Node(8);
-//        node.next.next.next.next.next.next.next.next = new Node(9);
-        //输入链表头节点，奇数返回中点，偶数返回上中点
-        Node ans1 = midOrUpMidNode(node);
-        System.out.println(ans1);
-        //输入链表头节点，奇数返回中点，偶数返回中下点
-        ans1 = midOrDownMidNode(node);
-        System.out.println(ans1);
-        //输入链表头节点，奇数返回中点上一个，偶数返回中点前一个
-        ans1 = midOrUpMidPreNode(node);
-        System.out.println(ans1);
-        //输入链表头节点，奇数返回中点上一个，偶数返回中点下一个
-        ans1 = midOrDownMidPreNode(node);
-        System.out.println(ans1);
-    }
-
-    private static Node midOrDownMidPreNode(Node node) {
-        if (node.next == null || node.next.next == null) {
-            return null;
-        }
-        Node slowNode = node;
-        Node fastNode = node.next.next;
-        while (fastNode.next != null && fastNode.next.next != null) {
-            slowNode = slowNode.next;
-            fastNode = fastNode.next.next;
-        }
-        return slowNode;
-    }
-
-    private static Node midOrUpMidPreNode(Node node) {
-        if (node.next == null || node.next.next == null) {
-            return null;
-        }
-        Node slowNode = node;
-        Node fastNode = node.next.next;
-        while (fastNode.next != null && fastNode.next.next != null) {
-            slowNode = slowNode.next;
-            fastNode = fastNode.next.next;
-        }
-        return slowNode;
-    }
-
-    private static Node midOrDownMidNode(Node node) {
-        if (node.next == null || node.next.next == null) {
-            return null;
-        }
-        Node slowNode = node.next;
-        Node fastNode = node.next;
-        while (fastNode.next != null && fastNode.next.next != null) {
-            slowNode = slowNode.next;
-            fastNode = fastNode.next.next;
-        }
-        return slowNode;
-    }
-
-    private static Node midOrUpMidNode(Node node) {
-        if (node.next == null || node.next.next == null) {
-            return null;
-        }
-        Node slowNode = node.next;
-        Node fastNode = node.next.next;
-        while (fastNode.next != null && fastNode.next.next != null) {
-            slowNode = slowNode.next;
-            fastNode = fastNode.next.next;
-        }
-        return slowNode;
-    }
-
-
     public static class Node {
-        public int id;
+        public int value;
         public Node next;
 
-        public Node(int id) {
-            this.id = id;
+        public Node(int v) {
+            value = v;
         }
 
         @Override
         public String toString() {
             return "Node{" +
-                    "id=" + id +
+                    "value=" + value +
                     '}';
         }
+
+    }
+
+    public static void main(String[] args) {
+        Node test = null;
+        test = new Node(0);
+        test.next = new Node(1);
+        test.next.next = new Node(2);
+        test.next.next.next = new Node(3);
+        test.next.next.next.next = new Node(4);
+        test.next.next.next.next.next = new Node(5);
+        test.next.next.next.next.next.next = new Node(6);
+        test.next.next.next.next.next.next.next = new Node(7);
+        test.next.next.next.next.next.next.next.next = new Node(8);
+        //输入链表头节点，奇数长度返回中点，偶数返回上中点
+        System.out.println(midOrUpMidNode(test));
+        //输入链表头节点，奇数长度返回中点，偶数返回下中点
+        System.out.println(midOrDownMidNode(test));
+        //输入链表头节点，奇数长度返回中点中点前一个，偶数返回上中点前一个
+        System.out.println(midOrUpMidPreNode(test));
+        //输入链表头节点，奇数长度返回中点中点前一个，偶数返回下中点前一个
+        System.out.println(midOrDownMidPreNode(test));
+
+    }
+    public static Node midOrDownMidPreNode(Node head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        if (head.next.next == null) {
+            return head;
+        }
+        Node slow = head;
+        Node fast = head.next;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    private static Node midOrUpMidPreNode(Node head) {
+        //链表必须有三个以上
+        if (head == null || head.next == null || head.next.next == null) {
+            return head;
+        }
+        Node slow = head;
+        Node fast = head.next;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    private static Node midOrDownMidNode(Node head) {
+        //链表必须有三个以上
+        if (head == null || head.next == null || head.next.next == null) {
+            return head;
+        }
+        Node slow = head.next;
+        Node fast = head.next;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public static Node midOrUpMidNode(Node head) {
+        //链表必须有三个以上
+        if (head == null || head.next == null || head.next.next == null) {
+            return head;
+        }
+        Node slow = head.next;
+        Node fast = head.next.next;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
     }
 }
